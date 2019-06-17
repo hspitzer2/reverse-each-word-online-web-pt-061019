59ae58b9bf624adfc9c267d674e0414712b99dde
Hillary Spitzer <hspitzer2@gmail.com>
2019-06-17 18:50:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,10 +2,10 @@ def reverse_each_word(string)
   string = ''
   reverse_string =[]
   when reverse_string. each do |char|
-    reverse_string.first.reverse.shift
+    reverse_string.first.reverse.unshift
     puts "#{char}"
   end
   til reverse_string.collect do |char|
-    "#{char}".first.reverse.shift
+    "#{char}".first.reverse.unshift
       end
     end
