810ff2cb67cd3671de0c17c22ab9f270d4324032
Hillary Spitzer <hspitzer2@gmail.com>
2019-06-17 19:59:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,11 +2,8 @@ def reverse_each_word(sentence)
   # turn your string into an array
   sentence_array = sentence.split
   reversed_sentence_array = []
-  sentence. each do |word|
-    # .first.reverse.unshift
-    puts "#{word}"
-  end
 
-#    til reverse_string.collect do |char|
-#      "#{char}".first.reverse.unshift
-#        end
+  sentence_array.each do |word|
+    reversed_sentence_array << word.reverse
+    reversed_sentence_array
+  end 
