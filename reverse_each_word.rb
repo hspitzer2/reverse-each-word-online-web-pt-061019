53294c12486bf2352d05e8aee6c6bf893b720f45
Hillary Spitzer <hspitzer2@gmail.com>
2019-06-17 18:47:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,11 @@
 def reverse_each_word(string)
   string = ''
   reverse_string =[]
-  reverse_string. each do |char|
-    reverse_string.shift
+  when reverse_string. each do |char|
+    reverse_string.first.reverse.shift
+    puts "#{char}"
+  end
+  til reverse_string.collect do |char|
+    "#{char}".first.reverse.shift
+      end
+    end
